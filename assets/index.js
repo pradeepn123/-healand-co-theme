46b63bb0af83304bfd3b32ba297ff30fadfd8746
hriks <hriks@outlook.com>
2023-09-05 18:51:25 +0530

    ✨ Able to Switch from OneTime to Subscription item

--- assets/index.js
+++ assets/index.js
@@ -7119,6 +7119,20 @@
                     })
                 }
             }, {
+                key: "getCustomConfigByItemIndexAndQuantity",
+                value: function(e, t, selling_plan) {
+                    const payload = {
+                        line: e + 1,
+                        quantity: t
+                    }
+                    if (selling_plan) {
+                        payload.selling_plan = selling_plan
+                    }
+                    return ce(m({}, no()), {
+                        body: JSON.stringify(payload)
+                    })
+                }
+            }, {
                 key: "updateItem",
                 value: function(e, t) {
                     var n = this,
@@ -7139,15 +7153,15 @@
                 key: "updateCustomItem",
                 value: function(e, t, selling_plan) {
                     var n = this,
-                        i = this.getConfigByItemIndexAndQuantity(e, t),
+                        i = this.getCustomConfigByItemIndexAndQuantity(e, t, selling_plan),
                         o = this.querySelector("#CartItem-".concat(e));
-                        console.log(o,'kiii')                        
-                    if (o) {                        
-                        var a = this._getCustomQueryConfig(e, t, selling_plan),
-                            r = new $i(o),
-                            s = +t; true ? (r.setLoading(!0), r.setDisable(!0), o.setQuantityDisabled(!0), 0 === s && (this.updateEmptyStatus(1 === this._cartItemCount), o.removeFromCart()), this._cartApi.change(a).then((function(e) {
-                            var t = JSON.parse(e);
-                            r.setLoading(!1), r.setDisable(!1), i.setLoading(!1), o.setQuantityDisabled(!1), n.updateByParsedState(t)
+                    if (o) {
+                        o.setLoading(1)
+                        var a = +t; true ? (0 === a && (this.updateEmptyStatus(1 === this._cartItemCount), o.removeFromCart()), this.setButtonLoading(!0), o.setQuantityDisabled(!0), this._cartApi.change(i).then((function() {
+                            return n._cartApi.get()
+                        })).then((function(e) {
+                            var t = me(e);
+                            n.setButtonLoading(!1), o.setQuantityDisabled(!1), o.setLoading(!1), n.updateByHTML(t)
                         })).catch((function() {
                             n._showError()
                         }))) : o.setLoading(!1)
